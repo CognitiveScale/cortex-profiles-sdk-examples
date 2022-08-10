b6f21a7420b7e6b655aef31258bf612f1d4a24c5
Luis Aguirre <laguirre@cognitivescale.com>
2022-08-10 11:55:15 -0500

    Add build.sh & c12e-ci.yml to run build with compatible jdk

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -eux
+
+function build() {
+  apt update &> /dev/null
+  apt install -q -y make &> /dev/null
+  make build create-app-image
+}
+
+function buildLocal() {
+  make build
+}
+
+### MAIN ####
+VERSION=$(git describe --long --always --match='v*.*' | sed 's/v//; s/-/./')
+echo "##### BUILDING ${VERSION} ######"
+case ${1-local} in
+ CI*)
+  build
+  ;;
+*)
+
+ buildLocal
+ ;;
+esac
