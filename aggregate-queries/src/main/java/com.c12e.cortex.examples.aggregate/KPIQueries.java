41f1028f65ea0b88b849c336cbe1afc6e797b828
Bikash Pandey <bpandey@cognitivescale.com>
2022-10-20 19:01:35 +0530

    Adding cohort filters, StartData, endDate and WindowDuration for the KPI script

--- aggregate-queries/src/main/java/com.c12e.cortex.examples.aggregate/KPIQueries.java
+++ aggregate-queries/src/main/java/com.c12e.cortex.examples.aggregate/KPIQueries.java
@@ -9,6 +9,12 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
 import com.c12e.cortex.phoenix.ProfileWindowedAggregation;
+import com.c12e.cortex.phoenix.ProfileScriptEngine;
+
+import java.sql.Timestamp;
+import java.time.LocalTime;
+import java.util.Collections;
+
 import static com.c12e.cortex.examples.aggregate.Metrics.setKPI;
 
 
@@ -24,28 +30,76 @@ public class KPIQueries implements Runnable {
     @Option(names = {"-ps", "--profile"}, description = "Profile Schema Name", required = true)
     String profileSchemaName;
 
+    @Option(names = {"-s", "--script"}, description = "KPI Script", required = true)
+    String script;
+
+    @Option(names = {"-d", "--duration"}, description = "Window Duration", required = true)
+    String windowDuration;
+
+    @Option(names = {"-cf", "--cohortFilters"}, defaultValue="", description = "Cohort Filter", required = false)
+    String[] cohortFilters;
+
+    @Option(names = {"-sd", "--startDate"}, defaultValue="", description = "Start Date, Set the time-frame over which the KPI is calculated", required = false)
+    String startDate;
+
+    @Option(names = {"-ed", "--endDate"}, defaultValue="", description = "End Date, Set the time-frame over which the KPI is calculated", required = false)
+    String endDate;
+
     @Override
     public void run() {
         var sessionExample = new SessionExample();
         CortexSession cortexSession = sessionExample.getCortexSession();
-        runKPI(cortexSession, project);
+        Double out = runKPI(cortexSession, project);
+        System.out.println("==========");
+        System.out.println(out);
+        System.out.println("==========");
     }
 
-    public boolean runKPI(CortexSession cortexSession, String project){
-        Dataset<Row> profileData = cortexSession.read().profile(project, profileSchemaName).load().toDF();
-        profileData = profileData.sample(0.002);
+    public String buildFilter(String[] cohortFilters, String startDate, String endDate) {
+        StringBuilder filterStr = new StringBuilder("");
+        if(!startDate.isBlank()){
+            filterStr.append("_timestamp.gte('" + Timestamp.valueOf(startDate) + "')");
+        }
+        if(!endDate.isBlank()) {
+            String filter = "_timestamp.lte('" + Timestamp.valueOf(endDate) + "')";
+            if (filterStr.toString().isBlank()) {
+                filterStr.append(filter);
+            } else {
+                filterStr.append(".and(" + filter + ")");
+            }
+        }
+        if (!String.join("",cohortFilters).isBlank()) {
+            String finalFilter = cohortFilters[0];
+            if (cohortFilters.length > 1) for(int i=1; i<cohortFilters.length; i=i+1) { finalFilter = finalFilter + ".or("+ cohortFilters[i] +")"; }
+            if (filterStr.toString().isBlank()) {
+                filterStr.append(finalFilter);
+            } else {
+                filterStr.append(".and("+ finalFilter +")");
+            }
+        }
+        return filterStr.toString();
+    }
 
-        String script = "filter(MAIL_STATUS.equalTo(\"MAIL CONTINUE\")).count()";
-//        profileData.filter(col("MAIL_STATUS").isin(new String[]{"MAIL CONTINUE"})).count()
-//        String script = "count()";
-        ProfileWindowedAggregation engine = new ProfileWindowedAggregation(profileData, "180 days");
+    public Dataset<Row> applyFilter(Dataset<Row> df, String filter) {
+        ProfileScriptEngine engine =  new ProfileScriptEngine(df, Collections.emptyMap());
+        return engine.applyFilter(filter);
+    }
+
+
+    public Double runKPI(CortexSession cortexSession, String project) {
+        Dataset<Row> profileData = cortexSession.read().profile(project, profileSchemaName).load().toDF();
+
+        ProfileWindowedAggregation engine = new ProfileWindowedAggregation(profileData, windowDuration);
+
+        if(!String.join("", cohortFilters).isBlank()) {
+            String filter = buildFilter(cohortFilters, startDate, endDate);
+            Dataset cohortData = applyFilter(profileData.toDF(), filter);
+            engine.addDataset("cohort", cohortData);
+        }
 
         AggregationResult result = engine.runWindowedAggregation(script);
-        Long firstRow = (Long) result.getDf().collectAsList().get(0).get(1);
-        System.out.println("========");
-        System.out.println(firstRow);
-        System.out.println("========");
-        setKPI(firstRow);
-        return true;
+        result.getDf().show();
+        Double KPI = Double.valueOf(result.getDf().select(result.getColName()).collectAsList().get(0).get(0).toString());
+        return KPI;
     }
 }
